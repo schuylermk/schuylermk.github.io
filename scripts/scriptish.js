9bfee9aae147717ef507de7f5a40421f53a32fc8
schuylermk <schuyler.m.k@gmail.com>
2017-07-20 23:35:52 -0700

    Document.ready

--- scripts/scriptish.js
+++ scripts/scriptish.js
@@ -1,6 +1,10 @@
+jQuery(document).ready(function($) {
+
 // Smooth scroll to header
-  $('.tothetop').click(function(){
-	$('html,body').animate({scrollTop: 0}, 500);
-	$(this).unbind("mouseenter mouseleave");
-      return false;
-  });
+    $('.tothetop').click(function(){
+        $('html,body').animate({scrollTop: 0}, 500);
+        $(this).unbind("mouseenter mouseleave");
+        return false;
+    });
+
+});
