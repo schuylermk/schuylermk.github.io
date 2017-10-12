a0a2c015834d65734d8647c8df295c6159cef97a
schuylermk <schuyler.m.k@gmail.com>
2017-10-11 23:54:38 -0700

    Remove unnec

--- scripts/scriptish.js
+++
@@ -1,9 +0,0 @@
-// $(document).ready(function($) {
-//     // alert( "ready!" );
-// // Smooth scroll to header
-//     $('#tothetop').click(function(topFunction){
-//         document.body.scrollTop = 0; // For Chrome, Safari and Opera 
-//         document.documentElement.scrollTop = 0;
-//     });
-
-// });
